b5f2989da540a78573041621f42a466ef5653867
AsPrus <krzysztof.prus1998@gmail.com>
2020-12-02 00:32:11 +0100

    ADD: project

---
+++ MultiplayerTest/Assets/controllBall.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class controllBall : NetworkBehaviour
+{
+    Transform _MoveableObject;
+    private void Start()
+    {
+        _MoveableObject = transform;
+    }
+
+    Vector3 move;
+
+    void Update()
+    {
+        if(isLocalPlayer)
+        {
+            move = new Vector3(
+                System.Convert.ToInt32(Input.GetKey(KeyCode.A)) - System.Convert.ToInt32(Input.GetKey(KeyCode.D)),
+                0,
+                System.Convert.ToInt32(Input.GetKey(KeyCode.S)) - System.Convert.ToInt32(Input.GetKey(KeyCode.W)));
+            
+            _MoveableObject.transform.Translate(move * Time.deltaTime);
+
+        }
+    }
+}
